0bd3e0a6e80f2c3a11fbeb83c638d59aa6a10339
Areeb <areeb.malik1989@gmail.com>
2019-05-12 10:33:46 +0530

    Initiallized Android app

---
+++ app/src/main/java/github/areebmalik1989/leafnotes/MainActivity.kt
@@ -0,0 +1,12 @@
+package github.areebmalik1989.leafnotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
