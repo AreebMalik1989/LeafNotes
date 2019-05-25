61980213ef396c9d52aff6e0113dd0b901fce2b9
Areeb <areeb.malik1989@gmail.com>
2019-05-25 14:07:31 +0530

    added usecase for deleting leafnote

---
+++ app/src/main/java/github/areebmalik1989/leafnotes/presenter/usecase/DeleteByIdPresenterUseCase.java
@@ -0,0 +1,27 @@
+package github.areebmalik1989.leafnotes.presenter.usecase;
+
+import github.areebmalik1989.core.domain.Identity;
+import github.areebmalik1989.core.usecase.leafnote.DeleteLeafNoteByIdUseCase;
+import github.areebmalik1989.core.usecase.leafnote.ILeafNoteRepository;
+
+public class DeleteByIdPresenterUseCase extends DeleteLeafNoteByIdUseCase {
+
+    public DeleteByIdPresenterUseCase(ILeafNoteRepository repository) {
+        super(repository);
+    }
+
+    public void deleteById(Identity id, Callback callback) {
+
+        callback.onUseCaseExecutionStart();
+
+        DeleteLeafNoteByIdUseCase.InputValues inputValues = new InputValues(id);
+        DeleteLeafNoteByIdUseCase.OutputValues outputValues = super.execute(inputValues);
+
+        callback.onUseCaseExecutionComplete(outputValues.isSuccess());
+    }
+
+    public interface Callback {
+        void onUseCaseExecutionStart();
+        void onUseCaseExecutionComplete(boolean success);
+    }
+}
