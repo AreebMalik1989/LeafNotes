ded0fd1149b3175943aa7d000cebe656e5055d1f
Areeb <areeb.malik1989@gmail.com>
2019-05-31 17:11:50 +0530

    added functionality for note

--- app/src/main/java/github/areebmalik1989/leafnotes/ui/main/MainActivity.kt
+++ app/src/main/java/github/areebmalik1989/leafnotes/ui/main/MainActivity.kt
@@ -1,11 +1,15 @@
 package github.areebmalik1989.leafnotes.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.annotation.LayoutRes
 import androidx.annotation.NonNull
 import androidx.fragment.app.Fragment
 import github.areebmalik1989.leafnotes.R
 import github.areebmalik1989.leafnotes.ui.BaseActivity
+import github.areebmalik1989.leafnotes.ui.note.NoteActivity
 
 class MainActivity : BaseActivity() {
 
@@ -18,9 +22,26 @@ class MainActivity : BaseActivity() {
         presenter = MainPresenter(fragment)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item?.itemId) {
+            R.id.new_note -> {
+                val intent = Intent(this@MainActivity, NoteActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     @LayoutRes
     override fun contentViewLayoutRes(): Int {
-        return R.layout.activity_main;
+        return R.layout.activity_main
     }
 
     @NonNull
---
+++ app/src/main/java/github/areebmalik1989/leafnotes/ui/note/NoteActivity.kt
@@ -0,0 +1,46 @@
+package github.areebmalik1989.leafnotes.ui.note
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import github.areebmalik1989.leafnotes.R
+import github.areebmalik1989.leafnotes.ui.BaseActivity
+
+class NoteActivity : BaseActivity() {
+
+    lateinit var presenter : NoteContract.Presenter
+    lateinit var fragment : NoteFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setTitle(R.string.edit_note)
+
+        presenter = NotePresenter(fragment)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_note, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item?.itemId) {
+            R.id.save_note -> {
+                presenter.saveNote()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun contentViewLayoutRes(): Int {
+        return R.layout.activity_note
+    }
+
+    override fun createInitialFragment(): Fragment {
+        fragment = NoteFragment.getInstance()
+        return fragment
+    }
+}
