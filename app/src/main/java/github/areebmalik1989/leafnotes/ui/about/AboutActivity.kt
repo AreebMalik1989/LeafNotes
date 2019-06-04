d69f4b486405cb5dc25a3880d8aa16a4d3dfee62
Areeb <areeb.malik1989@gmail.com>
2019-06-04 13:07:59 +0530

    added title in about activity

--- app/src/main/java/github/areebmalik1989/leafnotes/ui/about/AboutActivity.kt
+++ app/src/main/java/github/areebmalik1989/leafnotes/ui/about/AboutActivity.kt
@@ -1,10 +1,17 @@
 package github.areebmalik1989.leafnotes.ui.about
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import github.areebmalik1989.leafnotes.R
 import github.areebmalik1989.leafnotes.ui.BaseActivity
 
 class AboutActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setTitle(R.string.info)
+    }
 
     override fun contentViewLayoutRes(): Int {
         return R.layout.activity_about
